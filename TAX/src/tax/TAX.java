3f1da07ec152c92770b22062a26ca5a41bd67c37
ThomasVerth <Tverth2@gmail.com>
2020-02-27 16:04:02 -0500

    start of program

--- TAX/src/tax/TAX.java
+++ TAX/src/tax/TAX.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tax;
+import java.util.*;
+import java.io.*;
+ class lines {
+       public String line;
+       public int value;
 
-/**
- *
- * @author Tvert
- */
+   // constructor
+   public lines(String line, int value) {
+      this.line = line;
+      this.value = value;
+   }
+       public String getLine() { return line; }
+       public int getValue() { return value; }
+       // setter
+
+       public void setLine(String line) { this.line = line; }
+       public void setValue(int value) { this.value = value; }
+ }
 public class TAX {
-
-    /**
-     * @param args the command line arguments
-     */
+    static int line10100(){//Employment income
+        return 0;
+    }
+     
+        
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner k = new Scanner(System.in);
+       //fist page infomation below
+       //test code
+       lines[] arr = new lines[100];  // new stands for create an array object
+        arr[0] = new lines("Peter", 100); // new stands for create an employee object
+        arr[1] = new lines("Mary", 90);
+       System.out.println(arr[0].line);
+       //import a file of line number, initialize an array of structures
+       
+       
+       
     }
     
 }
